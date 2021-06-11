77bcb0ca3a95e268f8a17906cc9a03b7e84fcadc
Declimator <31232768+Declimator@users.noreply.github.com>
2021-06-10 17:00:29 -0700

    Factorial, StringTrimmer, RecursionMath IP

--- src/_01_StringTrimmer/StringTrimmer.java
+++ src/_01_StringTrimmer/StringTrimmer.java
@@ -7,13 +7,24 @@ public class StringTrimmer {
      * side of the specified string and returns the trimmed string. 
      */
     static public String trimString(String str, int removesLeft) {
+    	System.out.println("start");
         // 1. If there are no removes left
-            
+            if(removesLeft <= 0) {
+            	System.out.println("test");
+            	System.out.println(str);
+            	return str;
+            }
             // 2. Return the string
 
         // 3. Else you need to remove the last letter from the string,
         //    reduce removesLeft by 1, and then call trimString() 
-
-        return null;
+            else {
+            	str = str.substring(0, str.length()-1);
+            	System.out.println(str);
+            	str = trimString(str, removesLeft-1);
+            }
+        System.out.println("end" + removesLeft);
+        System.out.println(str);
+        return str;
     }
 }
